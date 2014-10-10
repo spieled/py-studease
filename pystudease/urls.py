2dbeb2486dd8958e6878b723f68fe86b63eaf643
lsp <spieled916@gmail.com>
2014-10-10 18:27:36 +0800

    1、配置url首页展示

--- pystudease/urls.py
+++ pystudease/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'practice.views.latest_questions'),
     url(r'^practice/', 'practice.views.latest_questions'),
 )
