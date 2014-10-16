021649937a37679aa256be090ffa552b0a9515e8
lsp <spieled916@gmail.com>
2014-10-16 10:56:15 +0800

    1、json问题调试

--- static/js/practice/verify.js
+++ static/js/practice/verify.js
@@ -7,8 +7,14 @@ $(function() {
                     success: function(responseText) {
                         var json = $.parseJSON(responseText);
                         var extra = '正确答案是：' ;
-                        var correct_answers = json.correct_answers || '';
-                        extra += correct_answers;
+                        var correct_answers = json.correct_answers || [];
+                        $.each(correct_answers, function(index, item) {
+                            var tag = $('INPUT[type=radio][name=answerId][value='+item+']').attr("tag");
+                            if (index > 0) {
+                                tag = "," + tag;
+                            }
+                            extra += tag
+                        });
 
                         $("#tipModel").find('SPAN[id=tipText]').text(json.msg + extra);
                         $("#tipModel").modal();
